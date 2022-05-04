748cf4fe1d7a88949e34a21d04d27fce3a1dec7e
easymean22 <dlwlals0414@naver.com>
2022-05-04 02:48:00 +0000

    FEAT: make python package

--- creataAPI/ifSpeed.py
+++ creataAPI/ifSpeed.py
@@ -14,21 +14,43 @@ def inSpeed():
     # get interface informatiom by snmp
     interfaces = findIndex()
 
+    # create value mapping
+    payload1 = {
+            "jsonrpc": "2.0",
+            "method": "valuemap.create",
+            "params": {
+                "hostid": HOST_ID,
+                "name": "Bandwidth",
+                "mappings": [
+                    {
+                        "type": "0",
+                        "value": "0",
+                        "newvalue": "unsupported"
+                    }
+                ]
+            },
+            "auth": AUTH,
+            "id": 1
+    }
+
+    response = requests.post(url, json=payload1).json()
+    valueMapID = response['result']['valuemapids'][0]
+
     # calculate intraffic for all interface 
     for interface in interfaces:
-        payload = {
+        payload2 = {
             "jsonrpc": "2.0",
             "method": "item.create",
             "params": {
                 "hostid" : HOST_ID,
-                "name": "In Error "+interface[1],
-                "key_": "interface."+interface[1].replace('/','')+".inerror",
+                "name": "Bandwidth "+interface[1],
+                "key_": "interface."+interface[1].replace('/','')+".bandwidth",
                 "value_type": 3, #numerinc unsigned
                 #"value_type": 4, #text
                 "type": 20, #snmp agent
                 "interfaceid": HOST_INTERFACEID,
-                "snmp_oid" : "1.3.6.1.2.1.2.2.1.14."+interface[0],
-                #"units" : "bps",
+                "snmp_oid" : "1.3.6.1.2.1.2.2.1.5."+interface[0],
+                "units" : "bps",
                 "delay" : "1m",
                 "history" : "7d",
                 "trends" : "0s",
@@ -39,22 +61,15 @@ def inSpeed():
                     },
                     {
                         "tag": "Interface Information",
-                        "value" : "in error"
+                        "value" : "bandwidth"
                     }
                 ],
-                "preprocessing": [ #get 'simple change' to catch a peak
-                    {
-                        "type": "9",
-                        "params": "",
-                        "error_handler": "1",
-                        "error_handler_params": ""
-                    }
-                ]
-                },
+                "valuemapid" : valueMapID
+            },
             "auth": AUTH,
             "id": 3
         }
-        response = requests.post(url, json=payload).json()
+        response = requests.post(url, json=payload2).json()
         print(json.dumps(response, indent=3, sort_keys=True))
 
 if __name__ == "__main__":
---
+++ init_zabbix.py
@@ -0,0 +1,8 @@
+import getAPI.host
+
+
+
+
+if __name__ == "__main__":
+    print(getAPI.host.findHost('PICOS switch1'))
+
